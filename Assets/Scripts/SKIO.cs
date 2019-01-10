e37568256944e3c114d466d1132ad7c43da498b7
Khiem NV <nvkhiem2016@gmail.com>
2019-01-10 10:06:53 +0700

    add scangame

--- Assets/Scripts/SKIO.cs
+++ Assets/Scripts/SKIO.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using SocketIO;
 public class SKIO : MonoBehaviour {
-
+    //test data
+    public string machineId="aaabbb"; //dùng để gửi id lên server
+    
+    public bool checkSetup = false;
     public static SocketIOComponent socket;
 
 
     public delegate void Event_Send_Result_Login(string _result,string _user);
-    public static Event_Send_Result_Login Evt_Send_Result_Login;    
+    public static Event_Send_Result_Login Evt_Send_Result_Login;
+    public delegate void Event_Send_MachineID(string _machineid, string _checksetup);
+    public static Event_Send_MachineID Evt_Send_MachineID;    
 	// Use this for initialization
 	void Start () {
+        machineId = "aaabbb";
+        Debug.Log(machineId);
         //socket.On
         socket.On("Server-App-Login", HandleLogin);
         socket.On("Server-App-SetTime", HandleSetTime);
         socket.On("Server-App-ChangePassword", HandleChangePassword);
         //socket.emit
-
         Evt_Send_Result_Login += Handler_Send_Result_Login;
-
+        Evt_Send_MachineID += Handler_Send_MachineID;
+        //tự emit
+        Evt_Send_MachineID(this.machineId, this.checkSetup.ToString());
 	}
     void Awake()
     {
@@ -26,7 +34,30 @@ public class SKIO : MonoBehaviour {
         DontDestroyOnLoad(socket);
     
     }
+    IEnumerator Coro_Send_MachineID(string _id, string _checksetup)//gửi id và checksetup lên sv
+    {
+        yield return new WaitForSeconds(0.1f);
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["id"] = _id;
+        data["checksetup"] = _checksetup;
+        if (SKIO.socket != null)
+        {
+           
+            SKIO.socket.Emit("App-Game-Send-MachineID", new JSONObject(data));
+        }
+    }
+    public void Handler_Send_MachineID(string _id, string _checksetup)
+    {
+        if (_id != null && _checksetup != null)
+        {
+
+            StartCoroutine(Coro_Send_MachineID(_id, _checksetup));
+
+        }
+
+    }
     //sự kiện socket.emit
+
     IEnumerator Coro_Send_Result_Login(string _result,string _user)
     {
         yield return new WaitForSeconds(0.1f);
